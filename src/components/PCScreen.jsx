ea39ed4efc0cbe15c349614e562314b54b7eda47
Md. Borhan Hosen <borhanhosen3@gmail.com>
2025-03-12 05:34:48 +0600

    sixth commit

--- src/components/PCScreen.jsx
+++ src/components/PCScreen.jsx
@@ -8,6 +8,7 @@ const socket = io(
 const PCScreen = () => {
   const [uniqueKey, setUniqueKey] = useState("");
   const [barcodes, setBarcodes] = useState([]);
+  const [products, setProducts] = useState([]);
 
   useEffect(() => {
     const generatedKey = Math.random().toString(36).substring(2, 10);
@@ -17,12 +18,26 @@ const PCScreen = () => {
 
     socket.on("receiveBarcode", (barcode) => {
       setBarcodes((prev) => [...prev, barcode]);
+      fetchProduct(barcode);
     });
 
     return () => {
       socket.off("receiveBarcode");
     };
   }, []);
+
+  const fetchProduct = async (barcode) => {
+    try {
+      const response = await fetch(
+        `https://wireless-barcode-scanner-backend-production.up.railway.app/products/${barcode}`
+      );
+      if (!response.ok) throw new Error("Product not found");
+      const data = await response.json();
+      setProducts((prev) => [...prev, data]);
+    } catch (error) {
+      setProducts([]);
+    }
+  };
 
   return (
     <div className="p-4">
@@ -31,9 +46,12 @@ const PCScreen = () => {
         Unique Key: <strong>{uniqueKey}</strong>
       </p>
       <ul className="mt-4">
-        {barcodes.map((barcode, index) => (
-          <li key={index} className="p-2 border-b">
-            {barcode}
+        {products.map((product, productIndex) => (
+          <li
+            key={` ${product.productName}-${productIndex}`}
+            className="p-2 border-b"
+          >
+            {product.productCode}-{product.productName}
           </li>
         ))}
       </ul>
